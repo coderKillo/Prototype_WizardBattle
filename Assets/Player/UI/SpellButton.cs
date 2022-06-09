874ff8042457a28f4f938c2b6b54d161cb88dde6
coderKillo <coderKillo@users.noreply.github.com>
2022-06-09 23:56:41 +0200

    add spellbar UI

---
+++ Assets/Player/UI/SpellButton.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class SpellButton
+{
+    public Sprite icon;
+    public String hotkey;
+}
